90e07040120efc6eb993b3615516c7e0e69fe07e
Macarealeportelli <macarenarealeportelli@gmail.com>
2021-02-10 19:52:46 -0300

    me conecto con la API de Marvel en JS

---
+++ js/index.js
@@ -0,0 +1,21 @@
+fetch("https://gateway.marvel.com/v1/public/comics?apikey=0b77a943b27f841e71a40bb1c01f879d")
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    console.log(data)
+    comics = data.data.results
+
+    const seccionTarjetas = document.querySelector(".contenedor-tarjetas");
+
+    comics.map((comic) => {
+
+      seccionTarjetas.innerHTML += `
+      <article>
+      <div class="image-comic"><img src="${comic.thumbnail.path}.jpg" alt=""></div>
+      <div class="comic-title">${comic.title}</div>
+      </article> 
+      `;
+
+    })
+  })
